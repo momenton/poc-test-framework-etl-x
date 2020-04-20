3738a4cc94271fd88ce39838ca99295f9c4a1ca0
Anshu <anshu.shukla@anz.com>
2020-04-20 10:58:45 +1000

    code cleanup

--- blackbox/test/customerData.test.js
+++ blackbox/test/customerData.test.js
@@ -1,47 +1,69 @@
 'use strict'
+
 const utils = require('@utils')
+const path = require('path')
 
-const srcOutputFile = './data/temp/output1.dat'
-const csvfileInput = './data/temp/input1.csv'
-const csvfileOutput = './data/temp/test_output1.csv'
-let inputfileArray = []
+const downlaodOptionsinput = {
+  destination: path.resolve('data/input.csv')
+}
+const downlaodOptionsoutput = {
+  destination: path.resolve('data/output.dat')
+}
+
+const inputFileName = 'customer_data/customer.csv'
+const outputFileName = 'customer_data/CUSTOMER.DAT'
+const srcOutputFile = path.resolve('data/output.dat')
+const csvfileInput = path.resolve('data/input.csv')
+const csvfileOutput = path.resolve('data/test_output.csv')
 let outputfileArr = []
+let inputfileArray = []
 let outputfileArray = []
-
-let rowsInput = 0
-let rowsOutput = 0
 
 describe('File comparison tests of customer file', () => {
   // prepare output.dat file for comparison
-  beforeAll(async () => {
-    utils.checkFile(csvfileOutput)
+  beforeAll(async done => {
+    jest.setTimeout(20000)
+    await utils.downloadFile(
+      process.env.GS_KEY,
+      process.env.GS_BUCKET_NAME,
+      inputFileName,
+      downlaodOptionsinput
+    )
+    await utils.downloadFile(
+      process.env.GS_KEY,
+      process.env.GS_BUCKET_NAME,
+      outputFileName,
+      downlaodOptionsoutput
+    )
+
     await utils.convertTocsv(srcOutputFile, csvfileOutput)
     inputfileArray = await utils.csvToArray(csvfileInput)
     outputfileArr = await utils.csvToArray(csvfileOutput)
     outputfileArray = utils.deleteRow(outputfileArr, outputfileArr.length)
     outputfileArray = await utils.arraySort(outputfileArray)
+    done()
   })
 
   test('date format in output header should be YYYYMMDD', () => {
     var datecheck = utils.validDateFormat(outputfileArray[0][1])
-
     expect(datecheck).toBeTruthy()
   })
+
   test('Header of output file "#|YYYYMMDD|no of records ,"', () => {
     const headerOutput = outputfileArray[0].toString()
     const expHeader =
       '#,' + outputfileArray[0][1] + ',' + (inputfileArray.length - 1)
     expect(headerOutput).toEqual(expHeader)
   })
+
   test('Number of records in Input and output file should be equal', () => {
-    rowsInput = inputfileArray.length
-    rowsOutput = outputfileArray.length
-    expect(rowsOutput).toBe(rowsInput)
+    expect(outputfileArray.length).toBe(inputfileArray.length)
   })
-  test('Number of columns in output file should be ten', () => {
+  test('Number of columns in output file should be 10', () => {
     const columnCheck = utils.numberOfColumns(outputfileArray, 10)
     expect(columnCheck).toBeTruthy()
   })
+
   test('mapping of input file to output file should be correct', () => {
     const mappingCheck = utils.mappingCustomer(
       10,
